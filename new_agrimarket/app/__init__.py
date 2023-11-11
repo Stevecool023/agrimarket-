03b496155764340cce88d68b3bc9a7ed5fd4d0f8
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-11 13:27:01 -0800

    init

--- new_agrimarket/app/__init__.py
+++ new_agrimarket/app/__init__.py
@@ -6,15 +6,19 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 
 def create_app():
-
     # Create the Flask application instance
     app = Flask(__name__)
+
+    # Configure the application
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'agrimarket.db')   # SQLite database file
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # Disable Flask-SQLAlchemy modification tracking
 
     # Initialize SQLAlchemy
-    db = SQLAlchemy(app)
+    db.init_app(app)
     migrate = Migrate(app, db)
 
-    # Import the routes module
-    from app import routes
+    # Import and register blueprints
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
