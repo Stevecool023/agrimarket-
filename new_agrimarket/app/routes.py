00df29765affa42bfb77c1a0d9e3ea48535a10b0
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-11 15:13:00 -0800

    routes

--- new_agrimarket/app/routes.py
+++ new_agrimarket/app/routes.py
@@ -31,3 +31,39 @@ def equipment():
 def add_product():
     # Your existing code for adding products
     pass
+
+@bp.route('/add_to_cart/<item_type>/<int:item_id>', methods=['POST'])
+def add_to_cart(item_type, item_id):
+    # Ensure the 'cart' key exists in the session
+    session.setdefault('cart', {})
+
+    # Increment the quantity or set it to 1 if the item is not in the cart
+    session['cart'][(item_type, item_id)] = session['cart'].get((item_type, item_id), 0) + 1
+
+    if item_type == 'product':
+        return redirect(url_for('products'))
+    elif item_type == 'equipment':
+        return redirect(url_for('equipment'))
+    else:
+        # Handle other item types as needed
+        pass
+
+
+@bp.route('/cart')
+def view_cart():
+    cart_contents = []
+
+    # Fetch details for items in the cart
+    for (item_type, item_id), quantity in session.get('cart', {}).items():
+        if item_type == 'product':
+            item = Product.query.get(item_id)
+        elif item_type == 'equipment':
+            item = Equipment.query.get(item_id)
+        else:
+            # Handle other item types as needed
+            item = None
+
+        if item:
+            cart_contents.append({'item': item, 'quantity': quantity})
+
+    return render_template('cart.html', cart_contents=cart_contents)
