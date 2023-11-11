29bee8f3cef0c408c83597be2748787b09bdefeb
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-11 13:50:32 -0800

    routes

--- new_agrimarket/app/routes.py
+++ new_agrimarket/app/routes.py
@@ -1,46 +1,33 @@
-#!/usr/bin/env python3
+# app/routes.py
 
-from flask import render_template, request, redirect, url_for
-from app import app, db
-from app.models import Product, BlogPost, Equipment, User
+from flask import Blueprint, render_template
+from app.models import Product, BlogPost, Equipment
 
-@app.route('/')
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
 def homepage():
     return render_template('index.html')
 
-@app.route('/products')
+@bp.route('/products')
 def products():
     # Fetch products from the database
     products_data = Product.query.all()
     return render_template('products.html', products=products_data)
 
-@app.route('/blog')
+@bp.route('/blog')
 def blog():
     # Fetch blog posts from the database
     blog_data = BlogPost.query.all()
     return render_template('blog.html', blog=blog_data)
 
-@app.route('/equipment')
+@bp.route('/equipment')
 def equipment():
     # Fetch equipment from the database
     equipment_data = Equipment.query.all()
     return render_template('equipment.html', equipment=equipment_data)
 
-@app.route('/add_product', methods=['GET', 'POST'])
+@bp.route('/add_product', methods=['GET', 'POST'])
 def add_product():
-    if request.method == 'POST':
-        # Get product details from the form
-        name = request.form['name']
-        description = request.form['description']
-
-        # Create a new product instance
-        new_product = Product(name=name, description=description)
-
-        # Add the product to the database
-        db.session.add(new_product)
-        db.session.commit()
-
-        # Redirect to the products page after adding the product
-        return redirect(url_for('products'))
-    
-    return render_template('add_products.html')
+    # Your existing code for adding products
+    pass
