2d9683804b5537254c68c5dd4fa18977a988d9b4
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-20 18:55:30 +0300

    manage app

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+from flask_migrate import Migrate
+from flask.cli import FlaskGroup
 from app import create_app, db
 
+app = create_app()
+migrate = Migrate(app, db)
+cli = FlaskGroup(create_app=create_app)
+
+cli.add_command("db", Migrate)
+
 if __name__ == '__main__':
-    app = create_app()
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    cli()
