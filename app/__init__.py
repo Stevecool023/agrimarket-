ea2ec45bebbb1e9a0cb7a9ab89d026441eec2a70
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-22 03:20:24 -0800

    app

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ from flask_migrate import Migrate
 import secrets
 from flask_login import LoginManager
 
+# Initialize the extensions without binding them to the app
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+
 def create_app():
     app = Flask(__name__)
 
@@ -14,12 +19,10 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'agrimarket.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db = SQLAlchemy(app)
-
-    migrate = Migrate(app, db)
-
-    login_manager = LoginManager(app)
-    login_manager.login_view = 'auth.login'
+    # Bind the extensions to the app inside the create_app function
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     from app.routes import main_bp, auth_bp
     app.register_blueprint(main_bp)
@@ -31,4 +34,7 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    return app, db
+    return app, db  # Return both app and db
+
+# Import the models after initializing db to avoid circular import
+from app.models import Item, User, BlogPost, Cart
