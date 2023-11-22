b9b1cab819cbc364a974d791659c32e509c04aa9
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-21 20:54:39 -0800

    flask logic

--- app/models.py
+++ app/models.py
@@ -1,66 +1,68 @@
-#!/usr/bin/env python3
+# models classes
 
-from app import db
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Float, text
+from flask_login import UserMixin
+from app import db
+
 
 class Item(db.Model):
-    __tablename__= 'item'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    image_url = db.Column(db.String(255))
+    item_type = db.Column(db.String(20), nullable=False)
+    code = db.Column(db.String(20), unique=True, nullable=True)
+    image_url = db.Column(db.String(100), nullable=True)
+    cost = db.Column(db.Float, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    item_type = db.Column(db.String(50), nullable=False)  # 'product' or 'equipment'
-    cost = db.Column(db.Float)
 
-    __mapper_args__ = {
-            'polymorphic_identity': 'item',
-            'polymorphic_on': item_type
-    }
+    def __repr__(self):
+        return f"Item(id={self.id}, name={self.name}, item_type={self.item_type}, code={self.code})"
 
 class Product(Item):
-    __tablename__ = 'product'
     id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
-    code = db.Column(db.String(255), nullable=False, unique=True)
-    image_url = db.Column(db.Text, nullable=False)
-    image_filename = db.Column(db.String(255), nullable=True)
+    __mapper_args__ = {'polymorphic_identity': 'product'}
+    __tablename__ = 'product'
 
-    __mapper_args__ = {
-            'polymorphic_identity': 'product',
-    }
+    def __repr__(self):
+        return f"Product(id={self.id}, name={self.name}, code={self.code})"
 
 class Equipment(Item):
-    __tablename__ = 'equipment'
     id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
-    code = db.Column(db.String(255), nullable=False, unique=True)
-    image_url = db.Column(db.Text, nullable=False)
-    image_filename = db.Column(db.String(255), nullable=True)
+    __mapper_args__ = {'polymorphic_identity': 'equipment'}
+    __tablename__ = 'equipment'
 
-    __mapper_args__ = {
-            'polymorphic_identity': 'equipment',
-    }
+    def __repr__(self):
+        return f"Equipment(id={self.id}, name={self.name}, code={self.code})"
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    cart_items = db.relationship('Cart', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username}, email={self.email})"
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
-
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"BlogPost(id={self.id}, title={self.title}, author_id={self.author_id})"
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    item_type = db.Column(db.String(50), nullable=False)  # 'product' or 'equipment'
-    item_id = db.Column(db.Integer, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    quantity = db.Column(db.Integer, default=1)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    item = db.relationship('Item', backref='cart_item', lazy=True)
+
+    def __repr__(self):
+        return f"Cart(id={self.id}, user_id={self.user_id}, item_id={self.item_id}, quantity={self.quantity})"
--- app/routes.py
+++ app/routes.py
@@ -1,111 +1,33 @@
-# app/routes.py
+# routes logic
 
-from flask import Blueprint, render_template, session, redirect, url_for, request
-from app.models import Product, BlogPost, Equipment, Cart
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, login_required, current_user, logout_user
 from app import db
-bp = Blueprint('main', __name__)
+from app.models import User, BlogPost, Item, Cart
+from app.routes import bp
 
 @bp.route('/')
-def homepage():
-    return render_template('index.html')
+def index():
+    products = Item.query.filter_by(item_type='product').all()
+    return render_template('index.html', products=products)
 
-@bp.route('/products')
-def products():
-    # Fetch products from the database
-    products = Product.query.all()
-    return render_template('products.html', products=products)
-
-@bp.route('/equipment')
-def equipment():
-    # Fetch equipment from the database
-    equipment = Equipment.query.all()
-    return render_template('equipment.html', equipment=equipment)
-
-@bp.route('/blog')
-def blog():
-    # Fetch blog posts from the database
-    blog_data = BlogPost.query.all()
-    return render_template('blog.html', blog=blog_data)
-
-@bp.route('/add_product_to_cart', methods=['POST'])
-def add_product_to_cart():
-    product_code = request.form.get('code')
-    quantity = int(request.form.get('quantity', 1))
-
-    # Fetch the product from the database based on the product code
-    product = Product.query.filter_by(code=product_code).first()
-
-    if product:
-        cart_item = Cart(
-            item_type='product',
-            item_id=product.id,
-            quantity=quantity,
-        )
-
+@bp.route('/add_to_cart/<int:item_id>')
+@login_required
+def add_to_cart(item_id):
+    item = Item.query.get(item_id)
+    if item:
+        cart_item = Cart(user=current_user, item=item)
         db.session.add(cart_item)
         db.session.commit()
+        flash(f"{item.name} added to cart successfully!", 'success')
+    else:
+        flash("Item not found.", 'danger')
+    return redirect(url_for('main.index'))
 
-        return redirect(url_for('main.view_cart'))
-
-    # Handle the case where the product is not found
-    return render_template('error.html', error_message='Product not found')
-
-@bp.route('/add_equipment_to_cart', methods=['POST'])
-def add_equipment_to_cart():
-    equipment_code = request.form.get('code')
-    quantity = int(request.form.get('quantity', 1))
-
-    # Fetch the equipment from the database based on the equipment code
-    equipment = Equipment.query.filter_by(code=equipment_code).first()
-
-    if equipment:
-        cart_item = Cart(
-            item_type='equipment',
-            item_id=equipment.id,
-            quantity=quantity,
-        )
-
-        db.session.add(cart_item)
-        db.session.commit()
-
-        return redirect(url_for('main.view_cart'))
-
-    # Handle the case where the equipment is not found
-    return render_template('error.html', error_message='Equipment not found')
-
-@bp.route('/cart')
+@bp.route('/view_cart')
+@login_required
 def view_cart():
-    cart_contents = []
+    user_cart_items = Cart.query.filter_by(user=current_user).all()
+    return render_template('cart.html', cart_items=user_cart_items)
 
-    # Check if 'cart' key exists in the session
-    cart_session = session.get('cart', {})
-
-    # Fetch details for items in the cart
-    for key, quantity in cart_session.items():
-        item_type, item_id = key.split('_')  # Split the string to retrieve type and id
-
-        if item_type == 'product':
-            item = Product.query.get(item_id)
-        elif item_type == 'equipment':
-            item = Equipment.query.get(item_id)
-        else:
-            # Handle other item types as needed
-            item = None
-
-        if item:
-            cart_contents.append({
-                'item': item,
-                'quantity': quantity,
-                'image_filename': getattr(item, 'image_filename', None),  # Get image filename from the form
-            })
-        else:
-            # Handle the case where the item is not found (deleted, etc.)
-            # You may want to remove it from the session or display a message
-            pass # Placeholder pass statement.
-
-    print(" Cart Contents:", cart_contents)  # Check cart contents in the console
-
-    # Print session data for debugging
-    print("Session Data:", cart_session)
-
-    return render_template('cart.html', cart_contents=cart_contents)
+# Add more routes as needed for other functionalities
