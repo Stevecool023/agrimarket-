a4c3067d7985abffd7d051c3e5fbd14e8c5a2752
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-22 02:02:29 -0800

    app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-# app/__init__.py
+# flask app
 
 import os
 from flask import Flask
@@ -6,30 +6,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import secrets
 from flask_login import LoginManager
-from app.models import User  # Move the import inside the function
+
+db = SQLAlchemy()
 
 def create_app():
-    # Create the Flask application instance
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = secrets.token_hex(16)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'agrimarket.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Configure the application
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'agrimarket.db')   # SQLite database file
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # Disable Flask-SQLAlchemy modification tracking
-
-    # Initialize SQLAlchemy
-    db = SQLAlchemy(app)
+    db.init_app(app)
     migrate = Migrate(app, db)
 
-    # Initialize Flask-Login
     login_manager = LoginManager(app)
-    login_manager.login_view = 'login' # 'login' is the endpoint for my login route
+    login_manager.login_view = 'login'
 
-    # Register blueprints
-    from app.routes import main_bp, auth_bp  # Importing inside the function
+    from app.routes import main_bp, auth_bp
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
+
+    from app.models import User  # Import inside the function
 
     @login_manager.user_loader
     def load_user(user_id):
